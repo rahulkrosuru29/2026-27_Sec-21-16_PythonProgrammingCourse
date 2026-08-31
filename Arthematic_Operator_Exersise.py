da1f2e4f884c6dbf33d910a1fede94659d67d275
rahulkrosuru29 <rahulkrosuru@gmail.com>
2026-08-31 15:57:57 +0530

    Athematic operations exercise

---
+++ Arthematic_Operator_Exersise.py
@@ -0,0 +1,24 @@
+#Arthematic_operators
+a = 10
+b = 5
+
+#Addition
+print("Sum of a and b is:", a + b) #Addition operator 
+
+# Subtraction
+print("Difference of a and b is:", a - b) # Subtraction operator
+
+# Multiplication
+print("Product of a and b is:", a * b) # Multiplication operator
+
+# Division
+print("Quotient of a and b is:", a / b) # Division operator
+
+# Modulus
+print("Remainder of a and b is:", a % b) # Modulus operator
+
+# Floor Division
+print("Floor division of a and b is:", a // b) # Floor division operator    
+
+# Exponentiation
+print("a raised to the power of b is:", a ** b) # Exponentiation operator
