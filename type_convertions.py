6be7753de011870f026f8c53cacf848dbda57892
rahulkrosuru29 <rahulkrosuru@gmail.com>
2026-09-16 20:56:15 +0530

    variable type convertions

---
+++ type_convertions.py
@@ -0,0 +1,13 @@
+# Type Conversion
+
+a = int(input("Enter an integer: "))
+print("float :",float(a))#int to float 
+print("str :",str(a))#int to string
+
+b = float(input("Enter a float: "))
+print("int :",int(b))#float to int
+print("str :",str(b))#float to string
+
+c = input("Enter a number as string: ")
+print("int :",int(c))#str to int
+print("float :",float(c))#str to float
