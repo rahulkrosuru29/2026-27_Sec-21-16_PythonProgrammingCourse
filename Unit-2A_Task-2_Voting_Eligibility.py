510488901438b53469b1afc6043b75de1631b3e9
rahulkrosuru29 <rahulkrosuru@gmail.com>
2026-09-25 14:36:12 +0530

    Voting Eligibility for 18+

---
+++ Unit-2A_Task-2_Voting_Eligibility.py
@@ -0,0 +1,6 @@
+#voting Eligibility
+Age_of_Citizen= int(input("Enter your age: "))
+if Age_of_Citizen >= 18:
+    print(f"You are eligible for Vote as your age is {Age_of_Citizen}")
+else:
+    print(f"You are not eligible for Vote as your age is {Age_of_Citizen}")
