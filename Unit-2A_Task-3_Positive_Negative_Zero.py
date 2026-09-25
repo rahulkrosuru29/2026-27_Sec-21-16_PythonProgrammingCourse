2a4d328e772b125e0712818b0d59344e212b59b6
rahulkrosuru29 <rahulkrosuru@gmail.com>
2026-09-25 14:54:43 +0530

    Positive Negative and Zero Determination

---
+++ Unit-2A_Task-3_Positive_Negative_Zero.py
@@ -0,0 +1,10 @@
+#Determining if a number is positive, negative, or zero
+Number = float(input("Enter a number: "))
+if Number > 0:
+    print(f"The number {Number} is positive.")#Positive number
+    print("Ruthwik is an konderripuku")
+elif Number < 0:
+    print(f"The number {Number} is negative.")#Negative number
+else:
+    print(f"The number {Number} is zero.")#Zero
+    
