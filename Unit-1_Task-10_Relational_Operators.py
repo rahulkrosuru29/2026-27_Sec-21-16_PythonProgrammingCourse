91977daebccfeedfad35fa2725dd3aa37ba481c3
rahulkrosuru29 <rahulkrosuru@gmail.com>
2026-09-25 14:38:38 +0530

    Update Unit-1_Task-10_Relational_Operators.py

--- Unit-1_Task-10_Relational_Operators.py
+++ Unit-1_Task-10_Relational_Operators.py
@@ -1,18 +1,15 @@
-#Membership operators
-#IN 
-a = [10, 20, 30, 40, 50]
-result = 30 in a
-print("result of 30 in a is:",result)
-
-b = {100, 200, 300, 400, 500}
-result = 600 in b
-print("result of 600 in b is:",result)
-
-#NOT IN
-c = ["Rahul", 500, "Diva", 700, 800]
-result = "RahulNRI" not in c
-print("result of 'RahulNRI' not in c is:",result)
-
-d = {"Brand": "Toyota", "year": 2000, "model": 3000,}
-result = "model" not in d
-print("result of 'model' not in d is:",result) 
+#python Relatioonal operators 
+a=29
+b=14
+result = a==b # equal to comparision 
+print("result of",a, "==",b,"is : ",result)
+result = a!=b #not equalto comparison
+print("result of",a, "!=",b,"is : ",result)
+result = a > b #greaterthan comparison
+print("result of",a, ">",b,"is : ",result)
+result = a < b #lessthan comparision
+print("result of",a, "<",b,"is : ",result)
+result = a >= b #greaterthan or equalto comparison
+print("result of",a, ">=",b,"is : ",result)
+result = a <= b #lessthan or equalto comparision
+print("result of",a, "<=",b,"is : ",result)
